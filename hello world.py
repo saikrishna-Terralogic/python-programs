0ae42f83d1ec8c2616f475f88d6c93537076be20
saikrishna <jillasai.krishna@nutanix.com>
2018-07-17 16:33:56 +0530

    added hello file

--- hello world.py
+++ hello world.py
@@ -1,18 +1,1 @@
-if 5>2:
-    print("hi")
-    '''This is a comment.
-    flajf'''
-age = 2
-Age = 3
-AGE = '1'
-print(Age)
-
-#print(Age+AGE)
-
-print(AGE)
-
-name = "    jilla sai    "
-print(name.replace('j', 'z'))
-print(dir(name))
-Ename  = input("enter the name")
-print("hello" +Ename)
+print("hello world")
